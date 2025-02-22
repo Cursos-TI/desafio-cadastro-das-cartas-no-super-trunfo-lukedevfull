3c1c40a3d0141bcc2cdbcf3956b281ee0dbe3f53
Luke_DEV <92399374+lukedevfull@users.noreply.github.com>
2025-02-22 13:11:08 -0300

    second version commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,88 +1,61 @@
 #include <stdio.h>
 
-#include <stdio.h>
+// Declaração das variáveis globais 
+char cardId[50];
+char states[50];
+char code[50];
+char city[50];
+int population;
+float area;
+float pib;  
+int numberOfTuristcPoints;
+
+// Declaração das funções !!(fora da main)!!
+void getCardDataInput() {
+    printf("Enter the card id: \n");
+    scanf("%49s", cardId); // Limita a 49 caracteres para evitar !!overflow
+    
+    printf("Enter the state: \n");
+    scanf("%49s", states);
+    
+    printf("Enter the card code: \n");
+    scanf("%49s", code);
+    
+    printf("Enter the city: \n");
+    scanf("%49s", city);
+    
+    printf("Enter the population: \n");
+    scanf("%d", &population);
+    
+    printf("Enter the area(KM): \n");
+    scanf("%f", &area);
+    
+    printf("Enter the PIB: \n");
+    scanf("%f", &pib);
+    
+    printf("Enter the number of tourist points in the city: \n");
+    scanf("%d", &numberOfTuristcPoints);
+}
+
+void printCardData() {
+    printf("\nThe card id: %s\n", cardId);
+    printf("The state: %s\n", states);
+    printf("Card code: %s\n", code);
+    printf("City: %s\n", city);
+    printf("Population: %d\n", population);
+    printf("Area: %.2f\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Number of tourist points: %d\n", numberOfTuristcPoints);
+}
+
 int main() {
-  // declare global variables
-  char cardId[50];
-  char states[50];
-  char code[50];
-  char city[50];
-  int population;
-  float area;
-  float pib;  
-  int numberOfTuristcPoints;
-  
-  // Get user-1 input
-  printf("Enter the card id: \n");
-  scanf("%s", cardId);
-  
-  printf("Enter the state: \n");
-  scanf("%s", states);
-  
-  printf("Enter the card code: \n");
-  scanf("%s", code);
-  
-  printf("Enter the city: \n");
-  scanf("%s", city);
-  
-  printf("Enter the population: \n");
-  scanf("%d", &population);
-  
-  printf("Enter the area(KM): \n");
-  scanf("%f", &area);
-  
-  printf("Enter the PIB: \n");
-  scanf("%f", &pib);
-  
-  printf("Enter the number of tourist points in the city: \n");
-  scanf("%d", &numberOfTuristcPoints);
-  
-  // Print the results
-  printf("Card code: %s\n", code);
-  printf("The state: %s\n", states);
-  printf("City: %s\n", city);
-  printf("Population: %d\n", population);
-  printf("Area: %.2f\n", area);
-  printf("PIB: %.2f\n", pib);
-  printf("Number of tourist points: %d\n", numberOfTuristcPoints);
-  
-  // Get user-2 input
-  printf("Enter the card id: \n");
-  scanf("%s", cardId);
-  
-  printf("Enter the state: \n");
-  scanf("%s", states);
-  
-  printf("Enter the card code: \n");
-  scanf("%s", code);
-  
-  printf("Enter the city: \n");
-  scanf("%s", city);
-  
-  printf("Enter the population: \n");
-  scanf("%d", &population);
-  
-  printf("Enter the area(KM): \n");
-  scanf("%f", &area);
-  
-  printf("Enter the PIB: \n");
-  scanf("%f", &pib);
-  
-  printf("Enter the number of tourist points in the city: \n");
-  scanf("%d", &numberOfTuristcPoints);
-  
-  
-  // Print the results -2
-  printf("The card id: %s\n", cardId);
-  printf("The state: %s\n", states);
-  printf("Card code: %s\n", code);
-  printf("City: %s\n", city);
-  printf("Population: %d\n", population);
-  printf("Area: %.2f\n", area);
-  printf("PIB: %.2f\n", pib);
-  printf("Number of tourist points: %d\n", numberOfTuristcPoints);
+    getCardDataInput(); // Chama a função para ler os dados
+    printCardData(); // Chama a função para exibir os resultados
 
-  return 0;
+    getCardDataInput();
+    printCardData();
+    // Finaliza o programa
+    return 0;
 }
 
-}
+//Tempo de porjeto 20min
